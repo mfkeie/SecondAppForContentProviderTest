4715f681381baa6c472795dc2a1bbf3bb7cd00ea
urakov.ivan <urakov.ivan@gmail.com>
2018-07-11 21:51:06 +0300

    20180711

--- app/src/main/java/elegion/com/secondappforcontentprovidertest/MainActivity.java
+++ app/src/main/java/elegion/com/secondappforcontentprovidertest/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinnerActions;
     private Button buttonAction;
     private EditText editTextId;
+    private EditText etVal1;
+    private EditText etVal2;
+    private EditText etVal3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         spinnerActions.setSelection(0);
 
         editTextId = findViewById(R.id.et_id);
+        etVal1 = findViewById(R.id.et_val1);
+        etVal2 = findViewById(R.id.et_val2);
+        etVal3 = findViewById(R.id.et_val3);
 
         buttonAction = findViewById(R.id.btn_action);
         buttonAction.setOnClickListener(new View.OnClickListener() {
@@ -72,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
                     case "insert" :
                         actionInsert(enity);
                         break;
-                   /* case "update" :
+                    case "update" :
                         actionUpdate(enity, id);
-                        break;*/
+                        break;
                     case "delete" :
                         actionDelete(enity, id);
                         break;
@@ -128,15 +134,15 @@ public class MainActivity extends AppCompatActivity {
     private void actionDelete(String enity, String id) {
         switch (enity) {
             case "Albums" :
-                getContentDelete("album", id,  "id", "name", "release");
+                getContentDelete("album", id);
                 break;
 
             case "Songs" :
-                getContentDelete("song", id,  "id", "name", "duration");
+                getContentDelete("song", id);
                 break;
 
             case "AlbumSongs" :
-                getContentDelete("albumsong", id,  "id", "album_id", "song_id");
+                getContentDelete("albumsong", id);
                 break;
             default:
                 break;
@@ -144,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getContentDelete(String table, String id, String col1, String col2, String col3) {
+    private void getContentDelete(String table, String id) {
         try {
             getContentResolver().delete(Uri.parse(URI_PREFIX + table + "/" + id), null, null);
         } catch (Exception ex) {
@@ -154,18 +160,18 @@ public class MainActivity extends AppCompatActivity {
     //<<Delete<<
 
     //>>Insert>>
-    private void actionInsert(String enity, String id) {
+    private void actionInsert(String enity) {
         switch (enity) {
             case "Albums" :
-                getContentInsert("album", id,  "id", "name", "release");
+                getContentInsert("album", "id", "name", "release");
                 break;
 
             case "Songs" :
-                getContentInsert("song", id,  "id", "name", "duration");
+                getContentInsert("song", "id", "name", "duration");
                 break;
 
             case "AlbumSongs" :
-                getContentInsert("albumsong", id,  "id", "album_id", "song_id");
+                getContentInsert("albumsong", "id", "album_id", "song_id");
                 break;
             default:
                 break;
@@ -173,21 +179,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getContentInsert(String table, String id, String col1, String col2, String col3) {
-        Cursor data = getContentResolver().insert(Uri.parse(TextUtils.isEmpty(id) ? URI_PREFIX + table : URI_PREFIX + table + "/" + id),
-                null);
-        if (data != null && data.moveToFirst()) {
-            StringBuilder builder = new StringBuilder();
-            do {
-                builder.append(data.getString(data.getColumnIndex(col1)) + " " +
-                        data.getString(data.getColumnIndex(col2)) + " " +
-                        data.getString(data.getColumnIndex(col3))
-                ).append("\n");
-            } while (data.moveToNext());
-            showText(builder.toString());
+    private void getContentInsert(String table, String col1, String col2, String col3) {
+        ContentValues values = new ContentValues();
+        values.put(col1, etVal1.getText().toString());
+        values.put(col2, etVal2.getText().toString());
+        values.put(col3, etVal3.getText().toString());
+
+        try {
+            getContentResolver().insert(Uri.parse(URI_PREFIX + table), values);
+        } catch (Exception e) {
+            showText(e.getMessage());
         }
+
     }
     //<<Insert<<
+
+    //>>Update>>
+    private void actionUpdate(String enity, String id) {
+        switch (enity) {
+            case "Albums" :
+                getContentUpdate("album", id,  "name", "release");
+                break;
+
+            case "Songs" :
+                getContentUpdate("song", id,  "name", "duration");
+                break;
+
+            case "AlbumSongs" :
+                getContentUpdate("albumsong", id, "album_id", "song_id");
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    private void getContentUpdate(String table, String id, String col2, String col3) {
+        ContentValues values = new ContentValues();
+        //values.put("id", etVal1.getText().toString());
+        values.put(col2, etVal2.getText().toString());
+        values.put(col3, etVal3.getText().toString());
+
+        try {
+            getContentResolver().update(Uri.parse(URI_PREFIX + table + "/" + id), values, null, null);
+        } catch (Exception e) {
+            showText(e.getMessage());
+        }
+
+    }
+    //<<Update<<
 
     private void showText(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
